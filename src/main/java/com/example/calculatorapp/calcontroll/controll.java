ee3cf6806bfd186b9cb823887e0f16eeb306c9f1
Zuhair Jarrar <es.zuhair.qussi2023@alexu.edu.eg>
2025-10-21 20:20:06 +0300

    the localy working version

--- src/main/java/com/example/calculatorapp/calcontroll/controll.java
+++ src/main/java/com/example/calculatorapp/calcontroll/controll.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/calculator")
-@CrossOrigin(origins="localhost:4200")
+@CrossOrigin(origins="http://localhost:4200")
 public class controll {
 
 
