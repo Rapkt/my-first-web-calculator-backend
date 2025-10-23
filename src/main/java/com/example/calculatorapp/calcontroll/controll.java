e3f0a675f0e7a959239f893f55de78a7c86e9855
Zuhair Jarrar <es.zuhair.qussi2023@alexu.edu.eg>
2025-10-23 19:22:27 +0300

    added some comment

--- src/main/java/com/example/calculatorapp/calcontroll/controll.java
+++ src/main/java/com/example/calculatorapp/calcontroll/controll.java
@@ -12,6 +12,7 @@ public class controll {
 
 
     @GetMapping("result")
+    // the main function
     public String readapi(@RequestParam(required = false) String expr){
         double number ;
         String[] expre = {"*","-","+","/","^","√","%"};
@@ -26,14 +27,18 @@ public class controll {
                         exp.push(x);
                         continue;
                     }
+                    //check if the power in the stack is higher than *
                     if(power(exp.peek())> power(x)){
+                        // calculate the numbers already in the stack
                         cal( exp,nums);
                     }
                     exp.push(x);
                     continue;
                 case "/":
+                    //check if the exp stack is empty
                     if(exp.empty()){
-                        if(Double.parseDouble(expresstion.get(i+1)) == 0){
+                        //check of we are dividing by 0 or if there is nothing to divide with
+                        if(i+1 > expresstion.size()-1 ||Double.parseDouble(expresstion.get(i+1)) == 0){
                             return "E";
                         }
                         exp.push(x);
@@ -48,6 +53,7 @@ public class controll {
                     exp.push(x);
                     continue;
                 case "-":
+                    //check if the next element in the expression is a number or an operator
                     boolean found=false;
                     for(int m =0; m < expre.length; m++ ){
                         if(i != 0){
@@ -59,16 +65,20 @@ public class controll {
                         }
                     }
                     if(exp.isEmpty()){
+                        //check if the next element is sqrt so we get the sqrt then we push the number in negative
                         if(expresstion.get(i+1).equals("√")){
                             nums.push(-1 * Math.sqrt(Double.parseDouble(expresstion.get(i+2))));
+                            // if the first number sqrt and in negative
                             if(i == 0){
                                 i+=2;
                                 continue;
                             }
                             i += 2;
+                            // we pushed the number in negative so we switch the operator to +
                             exp.push("+");
                             continue;
                         }
+                        // we push the number in negative then check if it was a sign or an operator
                         number = Double.parseDouble(expresstion.get(i+1));
                         nums.push((double)-1 * number);
                         if(i != 0){
@@ -77,8 +87,11 @@ public class controll {
                         i+=1;
                         continue;
                     }
+                    // if the - is an operator not a sign
                     if(!found) {
+                        // check if the power of the operator in the stack is grater than +
                         if (power("+") < power(exp.peek())) {
+                            // calculate the numbers in stack
                             cal(exp, nums);
                         }
                         if (i + 2 < expresstion.size() && expresstion.get(i + 1).equals("√")) {
@@ -133,25 +146,27 @@ public class controll {
                     nums.push(Math.sqrt(number));
                     i+=1;
                     continue;
-                case"s":
-                    number = nums.pop();
-                    nums.push(Math.sqrt(number));
-                    continue;
+
             }
             nums.push(Double.parseDouble(x));
         }
         return resultcalculated(exp,nums);
     }
+    // fucntion to read the input
     public List<String> input(String ex){
+        // array list for input
         List<String> tokens = new ArrayList<>();
+        // string to add in the array
         StringBuilder num = new StringBuilder();
 
         for (int i = 0; i < ex.length(); i++) {
             char c = ex.charAt(i);
-
+            //if the char is dot continue it is still the same number
             if (Character.isDigit(c) || c == '.') {
                 num.append(c);
-            } else if (c == '+' || c == '-' || c == '*' || c == '/' || c == 's' || c == '%' || c == '^' || c == '√') {
+            }
+            //there is an operation put it in the the next array index
+            else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^' || c == '√') {
                 if (num.length() > 0) {
                     tokens.add(num.toString());
                     num = new StringBuilder();
@@ -164,6 +179,7 @@ public class controll {
         }
         return tokens;
     }
+    // function to return the result
     public String resultcalculated(Stack<String> exp , Stack<Double> nums){
         while(!exp.empty()){
             double num2 = nums.pop();
@@ -192,7 +208,7 @@ public class controll {
         }
         return nums.pop().toString();
     }
-
+    //function to calculate the numbers in so we can continue with the expression
     public void cal( Stack<String> exp , Stack<Double> nums) {
         while (!exp.isEmpty()) {
             double num2 = nums.pop();
@@ -213,6 +229,7 @@ public class controll {
             }
         }
     }
+    //function to return the power
     public int power(String x){
         if(x.equals("+") || x.equals("-")){
             return 1;
